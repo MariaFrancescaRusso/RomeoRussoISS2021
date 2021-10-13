20cbaba962ea2a4bdaa38b1a5d0e1bbe787641c9
stefano <stefano.romeo@studio.unibo.it>
2021-10-13 16:15:21 +0200

    Project update test pantry and fridge

--- ProblemAnalysisModel/test/test/ResourceStateTest.kt
+++ ProblemAnalysisModel/test/test/ResourceStateTest.kt
@@ -15,76 +15,178 @@ import org.junit.After
 import org.junit.Assert.*
 import kotlin.jvm.JvmStatic
 import kotlinx.coroutines.channels.Channel
+import util.CoapObserverForTest
 
 class ResourceStateTest{
 		
 	companion object{
-		var myactor : ActorBasic? = null
+		var fridgeActor : ActorBasic? = null
+		var pantryActor : ActorBasic? = null
 		var systemStarted         = false
-		var testingObserver   : CoapObserverForTesting ? = null
+		var testingObserverFridge   : CoapObserverForTest ? = null
+		var testingObserverPantry 	: CoapObserverForTest ? = null
 		val channelSyncStart      = Channel<String>()
-	
 	
 		@JvmStatic
 		@BeforeClass
 		fun systemSetUp() {
 			println("===============TEST Init | Running context ")
+
 			GlobalScope.launch{ 
-				it.unibo.ctx.main()
+				it.unibo.ctxfridge.main()
 			}
+
+			GlobalScope.launch{ 
+				it.unibo.ctxmaitre.main()
+			}	
+/*					
+			GlobalScope.launch{ 
+				it.unibo.ctxrbr.main()
+			}
+*/									
 			println("===============TEST Init | Activating Observers")
+
 			GlobalScope.launch{
-				myactor=QakContext.getActor("rbr")
-				while(  myactor == null ){
+				fridgeActor=QakContext.getActor("fridge")
+				while(  fridgeActor== null ){
 					delay(500)
-					myactor=QakContext.getActor("rbr")
+					fridgeActor=QakContext.getActor("fridge")
 				}
-			channelSyncStart.send("starttesting")
+				channelSyncStart.send("starttesting1")
+			}
+			
+			GlobalScope.launch{
+				pantryActor=QakContext.getActor("pantry")
+				while(  pantryActor== null ){
+					delay(500)
+					pantryActor=QakContext.getActor("pantry")
+				}
+				channelSyncStart.send("starttesting2")
 			}
 		}
 			
-	    @After
+		@JvmStatic
+	    @AfterClass
 		fun terminate() {
-			println("===============TEST | terminate the testing")
-		}				
+			println("===============TEST | terminate the testing")				
+		}
 	}
 	
 	@Before
 	fun checkSystemStarted()  {
-	    println("testingObserver=$testingObserver")
+		var ip1 = "127.0.0.1"
+		var ip2 = "192.168.1.211"
+		var ctx1 = "ctxfridge"
+		var ctx2 = "ctxmaitre"
+		var actname1 = "fridge"
+		var actname2 = "pantry"
+		var port1 = "8060"
+		var port2 = "8070"
 		if( ! systemStarted ) {
 			runBlocking{
+				channelSyncStart.receive()
 				channelSyncStart.receive()
 				systemStarted = true
 				println("===============TEST | checkSystemStarted resumed ")
 				
-			}			
-		} 
-		if( testingObserver == null) testingObserver = CoapObserverForTesting("testingObserver","localhost", "ctxres", "pantry", "8080")
+			}
+		}
+		if( testingObserverFridge == null) testingObserverFridge = CoapObserverForTest("testingObserverFridge","$ip1", "$ctx1", "$actname1", "$port1")
+		if( testingObserverPantry == null) testingObserverPantry= CoapObserverForTest("testingObserverPantry","$ip2", "$ctx2", "$actname2", "$port2")
+	    println("testingObserverFridge=$testingObserverFridge")
+		println("testingObserverPantry=$testingObserverPantry")
   	}
 
+/*
 	@After
-	fun removeObs(){
-		println("${testingObserver!!.name}")
-		testingObserver!!.terminate()
-		testingObserver = null
+	fun removeObsFridge(){
+		println("${testingObserverFridge!!.name}")
+		testingObserverFridge!!.terminate()
+		testingObserverFridge = null
 	}
-    
+
+	@After
+	fun removeObsPantry(){
+		println("${testingObserverPantry!!.name}")
+		testingObserverPantry!!.terminate()
+		testingObserverPantry = null
+	}
+*/  
 	@Test
-	fun prepareTest(){
-		var prepare= MsgUtil.buildDispatch("tester", "prepare", "prepare(0)", "rbr")
-		var fridgeState = ""
-		var fridgePrevision = "State:c"
-		var expected = "State"
+	fun AddDishPantryTest(){
+		var	Dishes = arrayListOf(arrayListOf("dishes", "10"))
+		var Prevision = "Add Crockery [[dishes,10]] with success!"
+		var msg= MsgUtil.buildDispatch("tester", "changeState", "changeState(add, $Dishes)", "pantry")
+		var State = ""
+		var expected = "Add "
 		val channelForObserver = Channel<String>()		
-		testingObserver!!.addObserver( channelForObserver,expected )
+		testingObserverPantry!!.addObserver( channelForObserver,expected )
 		runBlocking{
-			println("===============TEST | sending $prepare")
-			MsgUtil.sendMsg(prepare, myactor!!)
+			println("===============TEST | sending $msg")
+			MsgUtil.sendMsg(msg, pantryActor!!)
+			State = channelForObserver.receive()			
+			
+			println("===============TEST | RESULT=$State for $msg")
+			assertEquals(Prevision,State)
+		}
+	}
+	
+	@Test
+	fun RemoveDishPantryTest(){
+		var	Dishes = arrayListOf(arrayListOf("dishes", "10"))
+		var Prevision = "Remove Crockery [[dishes,10]] with success!"
+		var msg= MsgUtil.buildDispatch("tester", "changeState", "changeState(remove, $Dishes)", "pantry")
+		var State = ""
+		var expected = "Remove "
+		val channelForObserver = Channel<String>()		
+		testingObserverPantry!!.addObserver( channelForObserver,expected )
+		runBlocking{
+			println("===============TEST | sending $msg")
+			MsgUtil.sendMsg(msg, pantryActor!!)
+			State = channelForObserver.receive()			
+			
+			println("===============TEST | RESULT=$State for $msg")
+			assertEquals(Prevision,State)
+		}
+	}
+	
+	@Test
+	fun AddFoodFridgeTest(){
+		var	Food = arrayListOf(arrayListOf("s034", "cheddar", "10"))
+		var fridgePrevision = "Add Food [[s034,cheddar,10]] with success!"
+		var msg= MsgUtil.buildDispatch("tester", "changeState", "changeState(add, $Food)", "fridge")
+		var fridgeState = ""
+		var expected = "Add "
+		val channelForObserver = Channel<String>()		
+		testingObserverFridge!!.addObserver( channelForObserver,expected )
+		runBlocking{
+			println("===============TEST | sending $msg")
+			MsgUtil.sendMsg(msg, fridgeActor!!)
 			fridgeState = channelForObserver.receive()			
 			
-			println("===============TEST | RESULT=$fridgeState for $prepare")
+			println("===============TEST | RESULT=$fridgeState for $msg")
 			assertEquals(fridgePrevision,fridgeState)
 		}
 	}
-}
+	
+	@Test
+	fun RemoveFoodFridTest(){
+		//var	PrepareDish = arrayListOf(arrayListOf("dishes", "10"), arrayListOf("glasses", "10")) 
+		var	Food = arrayListOf(arrayListOf("s001", "bread", "10"))
+		var fridgePrevision = "Remove Food [[s001,bread,10]] with success!"
+//		var fridgePrevision = "Remove Food $Food with success!"
+		var msg= MsgUtil.buildDispatch("tester", "changeState", "changeState(remove, $Food)", "fridge")
+		var fridgeState = ""
+		var expected = "Remove "
+		val channelForObserver = Channel<String>()		
+		testingObserverFridge!!.addObserver( channelForObserver,expected )
+		runBlocking{
+			println("===============TEST | sending $msg")
+			MsgUtil.sendMsg(msg, fridgeActor!!)
+			fridgeState = channelForObserver.receive()			
+			
+			println("===============TEST | RESULT=$fridgeState for $msg")
+			assertEquals(fridgePrevision,fridgeState)
+		}
+	}
+ }
