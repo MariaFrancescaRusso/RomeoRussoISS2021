fc98a6d41180daa84fb5241b305e58564fd795af
stefano <stefano.romeo@studio.unibo.it>
2022-01-29 21:41:50 +0100

    add rbr project

---
+++ Sprint_4/it.unibo.rbr/IsMap.pl
@@ -0,0 +1,12 @@
+%% Boolean value of map represented with a boolean value which is
+%% true if the room has been already mapped, otherwise false:
+%% 	 isMap(VALUE).
+
+%% ONLY for "https://swish.swi-prolog.org/"
+%:- dynamic (isMap/1).
+
+isMap(true).
+
+%% To obtain the value element
+getValue(Res) :-
+    findall([VALUE], isMap(VALUE), [[Res] | []]). 
