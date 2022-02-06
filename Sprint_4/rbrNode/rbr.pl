776dddef2bd266e2db9b22ee45cf124c7894bdb4
stefano <stefano.romeo@studio.unibo.it>
2022-02-06 04:10:43 +0100

    Sprint 4 yaml files working

---
+++ Sprint_4/rbrNode/rbr.pl
@@ -0,0 +1,15 @@
+%====================================================================================
+% rbr description   
+%====================================================================================
+context(ctxmaitre, "192.168.43.157",  "TCP", "8070").
+context(ctxfridge, "192.168.43.157",  "TCP", "8060").
+context(ctxrbr, "localhost",  "TCP", "8050").
+context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
+ qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( pantry, ctxmaitre, "external").
+  qactor( table, ctxmaitre, "external").
+  qactor( dishwasher, ctxmaitre, "external").
+  qactor( fridge, ctxfridge, "external").
+  qactor( rbr, ctxrbr, "it.unibo.rbr.Rbr").
+  qactor( rbrmapper, ctxrbr, "it.unibo.rbrmapper.Rbrmapper").
+  qactor( rbrwalker, ctxrbr, "it.unibo.rbrwalker.Rbrwalker").
