e545a80e11a735239e8db511b64a221d0f262181
stefano <stefano.romeo@studio.unibo.it>
2022-02-15 20:26:47 +0100

    fridge update

--- Sprint_4/fridgeNode/fridge.pl
+++ Sprint_4/fridgeNode/fridge.pl
@@ -1,5 +1,7 @@
 %====================================================================================
 % fridge description   
 %====================================================================================
+context(ctxmaitre, "192.168.1.211",  "TCP", "8070").
+context(ctxrbr, "192.168.1.93",  "TCP", "8050").
 context(ctxfridge, "localhost",  "TCP", "8060").
  qactor( fridge, ctxfridge, "it.unibo.fridge.Fridge").
